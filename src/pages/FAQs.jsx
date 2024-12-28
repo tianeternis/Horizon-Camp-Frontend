f6c5220fc9d7af309212f1a95c0d3164009f2595
Truong Toan <trgtoantyo3154@gmail.com>
2024-12-28 15:15:48 +0700

    feat: finish faqs page

--- src/pages/FAQs.jsx
+++ src/pages/FAQs.jsx
@@ -1,28 +1,26 @@
+import FAQsItem from "@/components/faqs/FAQsItem";
 import { useDynamicTitle } from "@/hooks";
 import BodyLayout from "@/layouts/BodyLayout";
 import { PATHS } from "@/routes";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const FAQs = ({}) => {
   const { t } = useTranslation();
 
   useDynamicTitle(t("title.faqs"));
 
-  const [isOpen, setIsOpen] = useState(false);
+  const faqs = t("faqs.content", { returnObjects: true });
 
   return (
     <div className="bg-white">
       <BodyLayout>
         <div className="space-y-12">
           <div className="space-y-4 text-center">
-            <h4 className="relative pb-2 text-3xl font-semibold before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-52 before:-translate-x-1/2 before:bg-main">
+            <h4 className="relative pb-2 text-2xl font-semibold before:absolute before:bottom-0 before:left-1/2 before:h-0.5 before:w-32 before:-translate-x-1/2 before:bg-main md:before:w-52 lg:text-3xl">
               {t("faqs.title")}
             </h4>
-            <p className="text-15px text-gray-800">
+            <p className="text-sm text-gray-800 lg:text-15px">
               {t("faqs.sub_title")}{" "}
               <Link
                 to={PATHS.contact()}
@@ -32,54 +30,27 @@ const FAQs = ({}) => {
               </Link>
             </p>
           </div>
-          <div className="w-full">
-            <div
-              className="flex w-full cursor-pointer flex-nowrap items-center rounded-md border border-solid border-[#badbcc] bg-[#d1e7dd] px-8 py-5 text-base font-semibold"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <div className="grow">Câu hỏi</div>
-              <span
-                className={`transform transition-transform duration-300 ${
-                  isOpen ? "rotate-180" : "rotate-0"
-                }`}
-              >
-                <IoIosArrowDown className="h-5 w-5" />
-              </span>
-            </div>
-            <AnimatePresence>
-              <motion.div
-                initial="hidden"
-                animate={isOpen ? "visible" : "hidden"}
-                exit="hidden"
-                variants={{
-                  hidden: {
-                    opacity: 0,
-                    height: 0,
-                    visibility: "hidden",
-                    marginTop: "0",
-                    transition: {
-                      opacity: { duration: 0.2 },
-                      height: { duration: 0.3 },
-                    },
-                  },
-                  visible: {
-                    opacity: 1,
-                    height: "auto",
-                    visibility: "visible",
-                    marginTop: "8px",
-                    transition: {
-                      opacity: { duration: 0.2 },
-                      height: { duration: 0.3 },
-                    },
-                  },
-                }}
-                className="w-full overflow-hidden will-change-auto"
-              >
-                <div className="w-full rounded-md border border-solid border-main/10 bg-main/5 px-8 py-5 text-[15px]">
-                  Câu trả lời
+          <div className="w-full space-y-12">
+            {faqs.length > 0 &&
+              faqs.map((item, i) => (
+                <div key={`faqs-${i}`} className="space-y-8">
+                  <div className="flex items-center gap-4">
+                    <span className="shrink-0 grow text-lg font-semibold uppercase md:text-xl">
+                      {item.title}
+                    </span>
+                    <div className="w-full border-t border-solid border-black/15"></div>
+                  </div>
+                  <div className="space-y-6">
+                    {item.questions.map((questionItem, index) => (
+                      <FAQsItem
+                        key={`faqs-question-${index}`}
+                        question={questionItem.question}
+                        answer={questionItem.answer}
+                      />
+                    ))}
+                  </div>
                 </div>
-              </motion.div>
-            </AnimatePresence>
+              ))}
           </div>
         </div>
       </BodyLayout>
