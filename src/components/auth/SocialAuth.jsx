e0bfff29e55d0572aa0b6c8487fbf3d0b53fc35d
Truong Toan <trgtoantyo3154@gmail.com>
2025-02-14 16:04:44 +0700

    feat: config firebase

--- src/components/auth/SocialAuth.jsx
+++ src/components/auth/SocialAuth.jsx
@@ -1,35 +1,35 @@
-import { FaFacebookF, FaGoogle } from "react-icons/fa";
+import { auth, google, signIn } from "@/configs/firebase";
 import { useTranslation } from "react-i18next";
+import { FcGoogle } from "react-icons/fc";
 
 const SocialAuth = () => {
   const { t } = useTranslation();
 
+  const handleLoginWithGoogle = async () => {
+    try {
+      const res = await signIn(auth, new google());
+
+      const { email, displayName } = res.user;
+    } catch (error) {
+      console.log("Sign in with google error: ", error);
+    }
+  };
+
   return (
-    <div className="w-full py-6">
-      <div className="mb-4 text-center text-13px text-gray-600 sm:flex sm:items-center sm:gap-4 sm:text-sm">
-        <div className="hidden w-full border-t border-solid border-black/10 sm:block"></div>
-        <span className="shrink-0 grow">{t("auth.login_with")}</span>
-        <div className="hidden w-full border-t border-solid border-black/10 sm:block"></div>
+    <div className="w-full py-6 sm:pt-0">
+      <div className="mb-4 flex items-center gap-6 px-3 text-center text-13px text-gray-600 sm:px-0 sm:text-sm">
+        <div className="w-full border-t border-solid border-black/10"></div>
+        <span className="shrink-0 grow">{t("auth.or")}</span>
+        <div className="w-full border-t border-solid border-black/10"></div>
       </div>
-      <div className="mx-auto flex w-72 flex-nowrap gap-2 sm:w-2/3 sm:px-6">
+      <div className="w-full px-3">
         <button
-          className={`flex w-1/2 flex-nowrap items-center rounded-sm bg-blue-600 text-white`}
+          className="mx-auto flex w-full flex-nowrap items-center justify-center gap-2.5 rounded-sm border border-solid border-gray-100 bg-transparent px-8 py-2.5 text-gray-800 hover:bg-gray-50 sm:w-6/10"
+          onClick={handleLoginWithGoogle}
         >
-          <span className="border-r border-solid border-white p-3">
-            <FaFacebookF />
-          </span>
-          <span className="flex w-full justify-center text-sm font-medium">
-            Facebook
-          </span>
-        </button>
-        <button
-          className={`flex w-1/2 flex-nowrap items-center rounded-sm bg-red-600 text-white`}
-        >
-          <span className="border-r border-solid border-white p-3">
-            <FaGoogle />
-          </span>
-          <span className="flex w-full justify-center text-sm font-medium">
-            Google
+          <FcGoogle className="text-xl" />
+          <span className="text-sm font-medium">
+            {t("auth.login_with_google")}
           </span>
         </button>
       </div>
